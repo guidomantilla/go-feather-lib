20244c2e84b24eb34945b33258d8115dbb17f025
Guido M Mantilla T <guido.mantilla@yahoo.com>
2024-09-13 17:37:24 -0500

    updates

--- tools/mockgen.go
+++ tools/mockgen.go
@@ -7,4 +7,5 @@ package tools
 
 //go:generate mockgen -package datasource 	-destination ../pkg/datasource/mocks.go 		-source ../pkg/datasource/types.go
 //go:generate mockgen -package=security 	-destination ../pkg/security/mocks.go			-source ../pkg/security/types.go
+//go:generate mockgen -package=messaging 	-destination ../pkg/messaging/mocks.go 			-source ../pkg/messaging/types.go
 //go:generate mockgen -package=server 		-destination ../pkg/server/mocks.go 			github.com/qmdx00/lifecycle Server
