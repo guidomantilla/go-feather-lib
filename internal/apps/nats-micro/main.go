ca7bdbf1c9e629da648d2a09c8a00ce9da1cbf5e
Guido M Mantilla T <guido.mantilla@yahoo.com>
2024-09-14 22:45:11 -0500

    updates

--- internal/apps/nats-micro/main.go
+++ internal/apps/nats-micro/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/guidomantilla/go-feather-lib/pkg/common/log"
-	"github.com/guidomantilla/go-feather-lib/pkg/server"
-	"github.com/qmdx00/lifecycle"
 	"os"
 	"syscall"
+
+	"github.com/qmdx00/lifecycle"
+
+	"github.com/guidomantilla/go-feather-lib/pkg/common/log"
+	"github.com/guidomantilla/go-feather-lib/pkg/server"
 )
 
 func main() {
